1f72b9fd2462728ac8c82aecebe9080d996fa7f1
Maryprog <144013702+hrebenchukM@users.noreply.github.com>
2024-10-29 12:23:50 +0200

    ...

---
+++ StandartInterface/Inerface.cs
@@ -0,0 +1,415 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace StandartInterface
+{
+
+    class Person
+    {
+        //защищённые поля name (имя), surname (фамилия), age (возраст),phone(телефон);
+        protected string name;
+        protected string surname;
+        protected int age;
+        protected string phone;
+
+
+        //свойства Name, Surname, Age, Phone;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Surname
+        {
+            get
+            {
+                return surname;
+            }
+            set
+            {
+                surname = value;
+            }
+        }
+
+        public string Phone
+        {
+            get
+            {
+                return phone;
+            }
+            set
+            {
+                phone = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+
+        //конструктор по умолчанию 
+        public Person()
+        {
+        }
+        //конструктор с параметрами
+        public Person(string n, string s, int a, string p)
+        {
+            Console.WriteLine("Конструктор класса Person");
+            name = n;
+            surname = s;
+            age = a;
+            phone = p;
+
+        }
+
+        //метод Print для вывода информации на экран
+        public void Print()
+        {
+            Console.WriteLine("Name: {0}, Surname: {1}, Age: {2}, Phone: {3}", name, surname, age, phone);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+    class Student : Person , IComparable
+    {
+        //реализующий интерфейс IComparable
+        public int CompareTo(object obj)
+        {
+            if (obj is Student)
+                return Average.CompareTo((obj as Student).Average);
+
+            throw new NotImplementedException();
+        }
+        //вложенные классы, реализующие интерфейс IComparer (для сортировки по различным критериям). 
+        public class SortByAverage : IComparer
+        {
+            int IComparer.Compare(object obj1, object obj2)
+            {
+                if (obj1 is Student && obj2 is Student)
+                    return (obj2 as Student).Average.CompareTo((obj1 as Student).Average);
+
+                throw new NotImplementedException();
+            }
+        }
+        public class SortByNumber_of_group : IComparer
+        {
+            int IComparer.Compare(object obj1, object obj2)
+            {
+                if (obj1 is Student && obj2 is Student)
+                    return (obj1 as Student).Number_of_group.CompareTo((obj2 as Student).Number_of_group);
+
+                throw new NotImplementedException();
+            }
+        }
+
+
+        //защищённые поля average (средний балл), number_of_group (номер группы);
+
+        protected double average;
+        protected string number_of_group;
+
+        //свойства Average, Number_Of_Group;
+        public double Average
+        {
+            get
+            {
+                return average;
+            }
+            set
+            {
+                average = value;
+            }
+        }
+
+        public string Number_of_group
+        {
+            get
+            {
+                return number_of_group;
+            }
+            set
+            {
+                number_of_group = value;
+            }
+        }
+
+
+        //конструктор по умолчанию 
+        public Student()
+        {
+        }
+        //конструктор с параметрами
+        public Student(string n, string s, int a, string p, double average, string number_of_group)
+             : base(n, s, a, p)
+        {
+            Console.WriteLine("Конструктор класса Student");
+            this.average = average;
+            this.number_of_group = number_of_group;
+        }
+
+        //метод Print для вывода информации на экран;
+
+        public void Print()
+        {
+            base.Print();
+            Console.WriteLine("Average: {0}, Number of group: {1} ", average, number_of_group);
+        }
+    }
+
+
+    class Academy_Group: ICloneable
+    {
+        //реализует интерфейс ICloneable
+        public object Clone()
+        {
+            var clone = new Academy_Group();
+            foreach (Student student in students)
+            {
+                clone.Add(new Student(student.Name, student.Surname, student.Age, student.Phone, student.Average, student.Number_of_group));
+            }
+            return clone;
+            // return new Club(Name, City, Coach.Name);
+        }
+
+        //public object Clone()
+        //{
+        //    return this.MemberwiseClone(); // Поверхностное копирование
+        //}
+
+
+
+
+
+        // Creates and initializes a new ArrayList.
+        //ArrayList myAL = new ArrayList();
+        //необобщенная коллекция ArrayList вместо массива
+        private ArrayList students;//переменная students - ссылочная переменная, которая указывает на объект коллекции ArrayList, где хранятся ссылки на студентов.
+
+
+        //счётчик count количества студентов в группе;
+        public int Count
+        {
+            get
+            {
+                return students.Count; // Возвращает количество студентов в ArrayList
+            }
+        }
+
+        //конструктор по умолчанию;
+        public Academy_Group()
+        {
+            students = new ArrayList(); //проинициализировала ArrayList
+        }
+
+
+
+        //метод Add для добавления студентов в группу;
+        public void Add(Student student)
+        {
+            // myAL.Add("Hello");
+            students.Add(student); // добавления студента в ArrayList
+            Console.WriteLine("Added {0}.", student.Name);
+        }
+
+        // метод Remove для удаления студента из группы (критерий удаления – фамилия);
+        public void Remove(string surname)
+        {
+            bool found = false;
+            for (int i = 0; i < students.Count; i++)
+            {
+                Student student = (Student)students[i];// извлечением итого объекта из ArrayList(ОБДЖЕКТЫ) и его приведением к типу Student
+                if (student.Surname == surname)
+                {
+                    // Removes the element at index 5.
+                    //myAL.RemoveAt(5);
+                    students.RemoveAt(i);
+                    Console.WriteLine("After removing the student: {0}", surname);
+                    found = true;
+                    break;
+                }
+
+            }
+            if (!found)
+            {
+                Console.WriteLine("No such student: {0}", surname);
+            }
+        }
+
+        // метод Edit для редактирования сведений о студенте(критерий – фамилия студента);
+        public void Edit(string surname, Student newStudent)
+        {
+
+            bool found = false;
+            for (int i = 0; i < students.Count; i++)
+            {
+                Student student = (Student)students[i];// извлечением итого объекта из ArrayList(ОБДЖЕКТЫ) и его приведением к типу Student
+                if (student.Surname == surname)
+                {
+                    students[i] = newStudent;
+                    Console.WriteLine("After editing the student: {0}", surname);
+                    found = true;
+                    break;
+                }
+
+            }
+            if (!found)
+            {
+                Console.WriteLine("No such student: {0}", surname);
+            }
+        }
+
+
+
+        //метод Sort для сортировки по заданному критерию; 
+        public void Sort(IComparer icomparer)
+        {
+            students.Sort(icomparer);
+
+            //// создаем клон группы
+            //var clone = Clone() as Academy_Group;
+            //// сортируем студентов во временной группе
+            //clone.students.Sort(icomparer);
+
+            //// очищаем оригинал и добавляем студентов после сорт
+            //students.Clear();
+            //foreach (Student student in clone.students)
+            //{
+            //    students.Add(student);
+            //}
+        }
+
+
+
+
+
+        //метод печати группы Print;
+        public void Print()
+        {
+            Console.WriteLine("Students in the group:");
+            //форич предпочтительнее в принте в отличии от фор ибо нам не нужно контролировать индексы и менять коллекцию, а просто нужен принт
+            foreach (Student student in students)
+            {
+                student.Print();
+            }
+        }
+
+        //метод Save для сохранения данных в файл;
+        public void Save(string path)
+        {
+            StreamWriter sw = new StreamWriter(path, false);
+            try
+            {
+                foreach (Student student in students)
+                {
+                    sw.WriteLine(student.Name);
+                    sw.WriteLine(student.Surname);
+                    sw.WriteLine(student.Age);
+                    sw.WriteLine(student.Phone);
+                    sw.WriteLine(student.Average);
+                    sw.WriteLine(student.Number_of_group);
+                    sw.WriteLine();//пустая строка разделитель
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка сохранения: {0}", e.Message);
+            }
+            finally
+            {
+                sw.Close();
+            }
+        }
+
+
+
+        //метод Load для загрузки данных из файла;
+        public void Load(string path)
+        {
+            StreamReader sr = new StreamReader(path);
+            try
+            {
+                string line;//текущая строка 
+                while ((line = sr.ReadLine()) != null)
+                {
+
+                    Student student = new Student
+                    {
+                        Name = line,//Мария
+                        Surname = sr.ReadLine(),//Гребенчук
+                        Age = Convert.ToInt32(sr.ReadLine()),//19
+                        Phone = sr.ReadLine(),//380972860462
+                        Average = Convert.ToDouble(sr.ReadLine()),//9
+                        Number_of_group = sr.ReadLine()//P222
+                    };
+
+
+                    students.Add(student);
+                    sr.ReadLine();//пропуск пустой строки
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка загрузк: {0}", e.Message);
+            }
+            finally
+            {
+                sr.Close();
+            }
+        }
+
+        //метод Search для поиска студента по заданному критерию
+        public void Search(string surname)
+        {
+            bool found = false;
+            //форич предпочтительнее в поиске в отличии от фор ибо нам не нужно контролировать индексы и менять коллекцию, а просто нужен принт поиска
+
+            foreach (Student student in students)
+            {
+                if (student.Surname == surname)
+                {
+                    Console.WriteLine("After searching the student: {0},info about him:", surname);
+                    student.Print();
+                    found = true;
+                    break;
+                }
+
+            }
+            if (!found)
+            {
+                Console.WriteLine("No such student: {0}", surname);
+            }
+        }
+
+
+
+    }
+}
---
+++ StandartInterface/Program.cs
@@ -0,0 +1,143 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+using StandartInterface;
+using System.Collections;
+using System;
+using System.Threading;
+
+
+// Клиентская часть приложения
+class Client
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Academy_Group group = new Academy_Group();
+
+            while (true)
+            {
+
+
+                Console.WriteLine("1.Добавить студента");
+                Console.WriteLine("2.Удалить студента");
+                Console.WriteLine("3.Редактировать студента");
+                Console.WriteLine("4. Поиск студента");
+                Console.WriteLine("5.Распечатать группу");
+                Console.WriteLine("6.Записать в файл");
+                Console.WriteLine("7. Прочитать из файла");
+                Console.WriteLine("8. Отсортировать студентов по среднему балу/номеру группы");
+                Console.WriteLine("0.Выход");
+                Console.Write("Выберите пунктик: ");
+
+                string words = Console.ReadLine();
+
+                switch (words)
+                {
+                    case "1"://1.Добавить студента
+                        Console.Write("Введите имя: ");
+                        string nameA = Console.ReadLine();
+                        Console.Write("Введите фамилию: ");
+                        string surnameA = Console.ReadLine();
+                        Console.Write("Введите возраст: ");
+                        int ageA = int.Parse(Console.ReadLine());
+                        Console.Write("Введите телефон: ");
+                        string phoneA = Console.ReadLine();
+                        Console.Write("Введите средний бал: ");
+                        double averageA = double.Parse(Console.ReadLine());
+                        Console.Write("введите номер группы: ");
+                        string number_of_groupA = Console.ReadLine();
+                        group.Add(new Student(nameA, surnameA, ageA, phoneA, averageA, number_of_groupA));
+                        break;
+
+                    case "2"://Удалить студента
+                        Console.Write("Введите фамилию: ");
+                        string surnameR = Console.ReadLine();
+                        group.Remove(surnameR);
+                        break;
+
+                    case "3"://Редактировать студента
+
+                        Console.Write("Введите фамилию студента которого будем редактировать: ");
+                        string surnameE = Console.ReadLine();
+
+                        Console.Write("Введите новое имя: ");
+                        string nameE = Console.ReadLine();
+                        Console.Write("Введите новую фамилию: ");
+                        string surnameE2 = Console.ReadLine();
+                        Console.Write("Введите новый возраст: ");
+                        int ageE = int.Parse(Console.ReadLine());
+                        Console.Write("Введите новый телефон: ");
+                        string phoneE = Console.ReadLine();
+                        Console.Write("Введите новый средний бал: ");
+                        double averageE = double.Parse(Console.ReadLine());
+                        Console.Write("введите новый номер группы: ");
+                        string number_of_groupE = Console.ReadLine();
+                        group.Edit(surnameE, new Student(nameE, surnameE2, ageE, phoneE, averageE, number_of_groupE));
+                        break;
+
+                    case "4"://Поиск студента
+                        Console.Write("Введите фамилию: ");
+                        string surnameS = Console.ReadLine();
+                        group.Search(surnameS);
+                        break;
+
+                    case "5"://Распечатать группу
+                        group.Print();
+                        break;
+
+
+                    case "6"://Записать в файл
+                        Console.Write("Записать файл: ");
+                        group.Save("../../../Data/group.txt");
+                        Console.WriteLine("сохранено.");
+
+                        break;
+
+                    case "7"://Прочитать из файла
+
+                        Console.Write("Прочитать файл: ");
+                        group.Load("../../../Data/group.txt");
+                        Console.WriteLine("Загружено:");
+                        break;
+                    case "8"://Отсортировать студентов по среднему балу/номеру группы
+                        Console.Write("Выберите пунктик критерия: ");
+                        Console.Write("\n1.Отсортировать студентов по среднему балу:");
+                        Console.Write("\n2.Отсортировать студентов по названию группы:\n");
+                        string choice = Console.ReadLine();
+                        IComparer icomparer = null;
+                        if (choice == "1")
+                        {
+                            icomparer = new Student.SortByAverage();
+                        }
+                        if (choice == "2")
+                        {
+                            icomparer = new Student.SortByNumber_of_group();
+                        }
+                        group.Sort(icomparer); 
+                        Console.WriteLine("Сортировка завершена.");
+                        break;
+
+                    case "0"://Выход
+                        return;
+
+                    default:
+                        Console.WriteLine("Ошибочка");
+                        break;
+                }
+
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("По окончании try-блока.");
+        }
+
+    }
+}
