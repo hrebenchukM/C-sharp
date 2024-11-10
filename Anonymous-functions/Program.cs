ed8534ac507c6f13f998e85105c689bedc6419c2
Maryprog <144013702+hrebenchukM@users.noreply.github.com>
2024-11-10 07:18:19 +0200

    ..

---
+++ Anonymous-functions/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading;
+// Иногда основанием для существования метода служит то обстоятельство, 
+// что он должен быть вызван только один раз посредством делегата. 
+// В подобных случаях можно воспользоваться анонимной функцией, чтобы не создавать отдельный метод.
+// Анонимная функция, по существу, представляет собой безымянный кодовый блок, 
+// передаваемый конструктору делегата. 
+// Преимущество анонимной функции состоит, в частности, в ее простоте. 
+// Благодаря ей отпадает необходимость объявлять отдельный метод, 
+// единственное назначение которого состоит в том, что он передается делегату.
+
+using Anonymous_functions;
+
+// Клиентская часть приложения
+class Client
+
+{
+    static void Main()
+    {
+        try
+        {
+
+            Tamagotchi tamagotchi = new Tamagotchi();
+
+            while (true)
+            {
+                Console.WriteLine("1.Запуск игры");
+                Console.WriteLine("0.Выход");
+                Console.Write("Выберите пунктик: ");
+
+                string words = Console.ReadLine();
+
+                switch (words)
+                {
+                    case "1":
+                        // Подписка на события с помощью лямбда-выражений анонимной функции
+                        tamagotchi.State += (message) =>
+                        {
+                            Console.WriteLine(message);
+                        };
+                        // Подписка на события с помощью делегата анонимной функции
+
+                        //tamagotchi.State += delegate (string message)
+                        //{
+                        //    Console.WriteLine(message);
+                        //};
+
+                        // Запуск игры
+                        tamagotchi.Play();
+                        break;
+
+
+                    case "0"://Выход
+                        return;
+
+                    default:
+                        Console.WriteLine("Ошибочка");
+                        break;
+                }
+
+
+
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("По окончании try-блока.");
+        }
+
+
+
+    
+    }
+}
+
+       
+    
---
+++ Anonymous-functions/Tamagotchi.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anonymous_functions
+{
+    public delegate void StateHandler(string message);
+
+    class Tamagotchi
+    {
+        private int health = 100;
+        private int hunger = 0;
+        private int happiness = 100;
+        private int sleep = 100;
+        private int illness = 0;
+
+        private string curReq;
+        private Random rnd = new Random();
+
+        public event StateHandler State;
+
+        public void Play()
+        {
+            while (health > 0)
+            {
+                Request();
+                Print();
+
+                Console.WriteLine("Тамагочи просит: {0}", curReq);
+                Console.WriteLine("Удовлетворить просьбу? (1-Да, 0-Нет): ");
+                int res = int.Parse(Console.ReadLine());
+
+                if (res == 1)
+                {
+                    Respond(true);
+                }
+                else
+                {
+                    Respond(false);
+                }
+
+                if (illness >= 3)
+                {
+                    illness++;
+                    health -= 10;
+                    Console.WriteLine("Тамагочи заболел слишком сильно и умер. Плохой Вы друг!!!");
+                    break; // Завершаем игру
+                }
+
+                Thread.Sleep(2000);
+            }
+        }
+
+        private void Respond(bool response)
+        {
+            hunger += 10;
+            sleep -= 10;
+            if (response)
+            {
+                illness = 0;
+
+                switch (curReq)
+                {
+                    case "покормить":
+                        hunger = 0;
+                        if (hunger > 100) hunger = 100;
+                        break;
+                    case "погулять":
+                        happiness = 100;
+                        if (happiness > 100) happiness = 100;
+                        break;
+                    case "усыпить":
+                        sleep = 100;
+                        break;
+                    case "полечить":
+                        illness = 0;
+                        health = 100;
+                        if (health > 100) health = 100;
+                        break;
+                    case "поиграть":
+                        happiness = 100;
+                        if (happiness > 100) happiness = 100;
+                        break;
+                }
+
+                State?.Invoke("Вы удовлетворили просьбу: " + curReq);
+            }
+            else
+            {
+                illness++;
+                health -= 33;
+                happiness -= 10;
+                State?.Invoke("Вы отказали в просьбе: " + curReq + " Ухудшение состояния...");
+            }
+        }
+
+        private void Request()
+        {
+            string[] req = { "покормить", "погулять", "усыпить", "полечить", "поиграть" };
+            string newReq;
+            do
+            {
+                newReq = req[rnd.Next(req.Length)];
+            } while (newReq == curReq);
+
+            curReq = newReq;
+        }
+
+        private void Print()
+        {
+
+            Console.WriteLine("        (\\_/)  ");
+            Console.WriteLine("       ( -o- ) ");
+            Console.WriteLine("      -(     )-  ");
+            Console.WriteLine("       ( ___ )  ");
+            Console.WriteLine("       _:   :_   ");
+
+            Console.WriteLine("Статус Тамагочи:");
+            Console.WriteLine("Здоровье: {0}%", health);
+            Console.WriteLine("Голод: {0}%", hunger);
+            Console.WriteLine("Счастье: {0}%", happiness);
+            Console.WriteLine("Сон: {0}%", sleep);
+            Console.WriteLine("Болезнь: {0}", illness);
+
+        }
+    }
+}
