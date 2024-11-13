d8e928b0833163dab7117d771e8645ab50ff1bcc
Maryprog <144013702+hrebenchukM@users.noreply.github.com>
2024-11-13 02:01:37 +0200

    LINQ. Упорядочивание. Группирование. Операции над множествами.

---
+++ LINQ Grouping/Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace LINQ
+{
+
+    class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public int DepId { get; set; }
+    }
+    class Department
+    {
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Создадим набор пользователей и выберем из них тех...
+            List<Department> departments = new List<Department>()
+            {
+                new Department(){ Id = 1, Country = "Ukraine", City = "Lviv" },
+                new Department(){ Id = 2, Country = "Ukraine", City = "Kyiv" },
+                new Department(){ Id = 3, Country = "France", City = "Paris" },
+                new Department(){ Id = 4, Country = "Ukraine", City = "Odesa" }
+            };
+
+            List<Employee> employees = new List<Employee>()
+            {
+            new Employee()
+            { Id = 1, FirstName = "Tamara", LastName = "Ivanova", Age = 22, DepId = 2 },
+            new Employee()
+            { Id = 2, FirstName = "Nikita", LastName = "Larin", Age = 33, DepId = 1 },
+            new Employee()
+            { Id = 3, FirstName = "Alica", LastName = "Ivanova", Age = 43, DepId = 3 },
+            new Employee()
+            { Id = 4, FirstName = "Lida", LastName = "Marusyk", Age = 22, DepId = 2 },
+            new Employee()
+            { Id = 5, FirstName = "Lida", LastName = "Voron", Age = 36, DepId = 4 },
+            new Employee()
+            { Id = 6, FirstName = "Ivan", LastName = "Kalyta", Age = 22, DepId = 2 },
+            new Employee()
+            { Id = 7, FirstName = "Nikita", LastName = " Krotov ", Age = 27, DepId = 4 }
+            };
+
+
+            Console.WriteLine("1) Упорядочить имена и фамилии сотрудников по алфавиту, которые\r\nпроживают в Украине. Выполнить запрос немедленно. ");
+            // Принцип Lazy Load заключается в том, что загрузка данных в память не происходит до момента, 
+            // пока они действительно не понадобятся.
+            // Lazy Load позволяет нам многократно использовать один и тот же запрос к одному источнику, 
+            // с гарантией получения самых актуальных данных.
+
+            // LINQ может быть выполнен немедленно. Для этого необходимо заключить весь запрос в скобки, и 
+            // вызвать один из методов преобразования ToList (), ToArray(), ToDictionary().
+
+            // Для сортировки набора данных по возрастанию используется оператор orderby
+            // Вместо оператора orderby можно использовать метод расширения OrderBy
+
+            //// 1 способ через LINQ
+            var result1 = (from employee in employees
+                          join department in departments on employee.DepId equals department.Id
+                          where department.Country == "Ukraine"
+                          orderby employee.FirstName, employee.LastName
+                          select new { employee.FirstName, employee.LastName }).ToList();
+
+            foreach (var employee in result1)
+            {
+                Console.WriteLine("{0} : {1}", employee.FirstName, employee.LastName);
+            }
+            Console.WriteLine();
+
+        //// 2 способ через методы расширения LINQ
+        //ThenBy: задает дополнительные критерии для упорядочивания элементов возрастанию
+
+        result1 = employees.Join(departments, employee => employee.DepId, department => department.Id, (employee, department) => new
+            {
+                employee.FirstName,
+                employee.LastName,
+                department.Country
+            })
+            .Where(x => x.Country == "Ukraine")
+            .OrderBy(x => x.FirstName)
+             .ThenBy(x => x.LastName)
+            .Select(x => new
+            {
+                x.FirstName,
+                x.LastName,
+            }).ToList();
+
+
+            foreach (var employee in result1)
+            {
+                Console.WriteLine("{0} : {1}", employee.FirstName, employee.LastName);
+            }
+            Console.WriteLine();
+
+
+
+
+
+
+            Console.WriteLine("2) Отсортировать сотрудников по возрастам по убыванию. Вывести Id,\r\nFirstName, LastName, Age. Выполнить запрос немедленно ");
+            //// 1 способ через LINQ
+            var result2 = (from employee in employees
+                           orderby employee.Age descending
+                           select new
+                           {
+                               employee.Id,
+                               employee.FirstName, 
+                               employee.LastName,
+                               employee.Age
+                           }).ToList();
+
+            foreach (var employee in result2)
+            {
+                Console.WriteLine("{0} : {1} : {2} : {3}", employee.Id, employee.FirstName, employee.LastName, employee.Age);
+            }
+            Console.WriteLine();
+
+            //// 2 способ через методы расширения LINQ
+            result2 = employees.OrderByDescending(employee => employee.Age)
+            .Select(employee => new
+            {
+                employee.Id,
+                employee.FirstName,
+                employee.LastName,
+                employee.Age
+            }).ToList();
+           
+            foreach (var employee in result2)
+            {
+                Console.WriteLine("{0} : {1} : {2} : {3}", employee.Id, employee.FirstName, employee.LastName, employee.Age);
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine("3) Сгруппировать студентов по возрасту. Вывести возраст и сколько раз он\r\nвстречается в списке. ");
+            // Результатом оператора group является выборка, которая состоит из групп.
+            // Выражение group department by department.Country into g определяет переменную g, которая будет содержать группу. 
+            // С помощью этой переменной мы можем затем создать новый объект анонимного типа: 
+            // select new { Name = g.Key, Count = g.Count() } 
+            // Теперь результат запроса LINQ будет представлять набор объектов таких анонимных типов, 
+            // у которых два свойства Name и Count
+
+
+            // 1 способ через LINQ
+            var result3 = from employee in employees
+                          group employee by employee.Age into g
+                          select new { Name = g.Key, Count = g.Count() };
+
+            foreach (var group in result3)
+            {
+                Console.WriteLine("{0} : {1}", group.Name, group.Count);
+            }
+            Console.WriteLine();
+
+
+            //// 2 способ через методы расширения LINQ
+            // Аналогичный запрос можно построить с помощью метода расширения GroupBy
+
+            result3 = employees.GroupBy(employee => employee.Age)
+                        .Select(g => new { Name = g.Key, Count = g.Count() });
+            foreach (var group in result3)
+                Console.WriteLine("{0} : {1}", group.Name, group.Count);
+            Console.WriteLine('\n');
+
+
+
+     
+        }
+    }
+}
--- String/Program.cs
+++ String/Program.cs
@@ -1,170 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
-using System.IO;
-namespace CSharp.String
+﻿using System;
+
+class A
 {
-    class MainClass
+    public A()
     {
-        static void Main()
-        {
-
-            try
-            {
-
-                //Console.Title = "The title has changed!";
-                //string s = "Это простая строка";
-                //Console.WriteLine(s);
-                //Console.BackgroundColor = ConsoleColor.Blue;
-                //Console.ForegroundColor = ConsoleColor.Yellow;
-                //Console.SetWindowSize(50, 30);
-                //string result = string.Format("Длина строки: {0}", s.Length);
-                //Console.WriteLine(result);
-
-                //string t = s.Substring(4, 7); // возвращает подстроку из 7 символов, начиная с 4 позиции
-                //Console.WriteLine(t);
-                //Console.WriteLine(s[5]);
-
-                //s = s.Replace("о", "О");
-                //Console.WriteLine(s);
-
-                //s = s.Remove(4, 8); // удаляет 8 символов, начиная с позиции 4
-                //Console.WriteLine(s);
-
-                ////str[5] = '!'; // недопустимо: доступ только на чтение
-                //char[] ar = { 'с', 'т', 'р', 'о', 'к', 'а' };
-
-                //string s2 = new string(ar);
-                //Console.WriteLine(s2);
-                //Console.WriteLine("Длина строки: " + s2.Length);
-
-                //string[] arstr = { " Platform ", " .NET ", " Framework " };
-                //for (int i = 0; i < arstr.Length; i++)
-                //{
-                //    Console.WriteLine(arstr[i]);
-                //}
-                //arstr[0] = " Common ";
-                //arstr[1] = " Language ";
-                //arstr[2] = " Runtime ";
-                //for (int i = 0; i < arstr.Length; i++)
-                //{
-                //    Console.WriteLine(arstr[i]);
-                //}
-
-                //Console.WriteLine("Введите первую строку: ");
-                //string s3 = Console.ReadLine();
-                //Console.WriteLine("Введите вторую строку: ");
-                //string s4 = Console.ReadLine();
-                //if (s3.CompareTo(s4) > 0)
-                //    Console.WriteLine(s3 + " больше, чем " + s4);
-                //else if (s3.CompareTo(s4) < 0)
-                //    Console.WriteLine(s4 + " больше, чем " + s3);
-                //else
-                //    Console.WriteLine("Строки равны");
-
-                //string text = "To be or not be";
-                //Console.WriteLine(" Индекс первого вхождения слова \"be\" = {0} ", text.IndexOf("be"));
-                //Console.WriteLine(" Индекс последнего вхождения слова \"be\" = {0} ", text.LastIndexOf("be"));
-
-                //Console.WriteLine();
-                //string words = "Эта    строка  будет  разбита  на  массив  строк";
-                //string[] arrayOfString = words.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                //foreach (string s5 in arrayOfString)
-                //{
-                //    Console.WriteLine(s5);
-                //}
-                //string res = string.Join(" ", arrayOfString);
-                //Console.WriteLine(res);
-
-                //string str1 = "Я ";
-                //string str2 = "учу ";
-                //string str3 = "C#";
-                //string str4 = str1 + str2 + str3;
-
-                //Console.WriteLine("{0} + {1} + {2} = {3}", str1, str2, str3, str4);
-
-                //str4 = str4.Replace("учу", "изучаю");
-                //Console.WriteLine(str4);
-
-                //str4 = str4.Insert(2, "упорно ").ToUpper();
-                //Console.WriteLine(str4);
-
-                //if (str4.Contains("упорно"))
-                //    Console.WriteLine("Учу таки упорно :)");
-                //else
-                //    Console.WriteLine("Учу как могу");
-
-
-
-
-
-                Console.WriteLine("Введите арифметическое выражение типо x + y - z:");
-                string words = Console.ReadLine();
-                string[] arrayOfString = words.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
-
-
-                if (arrayOfString.Contains("+") || arrayOfString.Contains("-"))
-                {
-
-                    int res = Convert.ToInt32(arrayOfString[0]);
-
-
-   
-                    string cur= "";
-
-                    foreach (string s5 in arrayOfString)
-                    {
-                        if (s5 == "+" || s5 == "-")
-                        {
-                            cur = s5;
-                        }
-                        else
-                        {
-                            int next = Convert.ToInt32(s5);
-                            if (cur == "+")
-                            {
-                                res += next;
-                            }
-                            else if (cur == "-")
-                            {
-                                res -= next;
-                            }
-                        }
-
-                    }
-
-                    Console.WriteLine("Результат: ");
-                    Console.WriteLine(res);
-
-
-                }
-
-            }
-
-
-           catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-
-        }
-
+        Console.WriteLine("A created");
     }
 }
 
+class B : A
+{
+    private B()
+    {
+        Console.WriteLine("B created");
+    }
+
+    public B(string parametr)
+    {
+        Console.WriteLine("B created with parametr: {0}", parametr);
+    }
+
+    public B()
+    {
+        Console.WriteLine("B created");
+    }
+}
+
+class C : B
+{
+    public C()
+    {
+        Console.WriteLine("C created");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        A ab = new B("inheritance");
+        A ac = new C();
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
+}
