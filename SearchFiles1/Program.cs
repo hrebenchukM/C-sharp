9edccb90e831291983dc5819b54ae9bb71e87def
Mary <marya101204@gmail.com>
2024-12-12 08:41:07 +0200

    ....

---
+++ SearchFiles1/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CSharpApplication.SearchInFiles
+{
+    // Класс для поиска текста в файлах, заданных DOS'вской маской
+    class Search
+    {
+        /*
+         class Directory https://msdn.microsoft.com/ru-ru/library/system.io.directory(v=vs.110).aspx
+         class DirectoryInfo https://msdn.microsoft.com/ru-ru/library/system.io.directoryinfo(v=vs.110).aspx
+         class File https://learn.microsoft.com/ru-ru/dotnet/api/system.io.file?view=net-6.0
+         class FileInfo https://msdn.microsoft.com/ru-ru/library/system.io.fileinfo(v=vs.110).aspx
+         */
+        static void Main()
+        {
+            Console.Write("Введите путь к каталогу: ");
+            string Path = Console.ReadLine();
+            Console.Write("Введите маску для файлов: ");
+            string Mask = Console.ReadLine();
+
+
+            Console.Write("Введите начальную дату (yyyy-mm-dd): ");
+            DateTime start;
+            if (!DateTime.TryParse(Console.ReadLine(), out start))
+            {
+                Console.WriteLine("Некорректная дата!");
+                return;
+            }
+
+
+
+            Console.Write("Введите конечную дату (yyyy-mm-dd): ");
+            DateTime end;
+
+            if (!DateTime.TryParse(Console.ReadLine(), out end))
+            {
+                Console.WriteLine("Некорректная дата!");
+                return;
+            }
+
+
+            //Console.Write("Введите текст для поиска в файлах: ");
+            //string Text = Console.ReadLine();
+
+            // Дописываем слэш (в случае его отсутствия)
+            if (Path[Path.Length - 1] != '\\')
+                Path += '\\';
+
+            // Создание объекта на основе введенного пути
+            DirectoryInfo di = new DirectoryInfo(Path);
+            // Если путь не существует
+            if (!di.Exists)
+            {
+                Console.WriteLine("Некорректный путь!!!");
+                return;
+            }
+
+            // Преобразуем введенную маску для файлов 
+            // в регулярное выражение
+
+            // Заменяем . на \.
+            Mask = Mask.Replace(".", @"\.");
+            // Заменяем ? на .
+            Mask = Mask.Replace("?", "."); // ????.txt  ....\.txt
+            // Заменяем * на .*
+            Mask = Mask.Replace("*", ".*");// *.txt   .*\.txt
+            // Указываем, что требуется найти точное соответствие маске
+            Mask = "^" + Mask + "$";
+
+            // Создание объекта регулярного выражения
+            // на основе маски
+            Regex regMask = new Regex(Mask, RegexOptions.IgnoreCase);
+
+            // Экранируем спецсимволы во введенном тексте
+            //Text = Regex.Escape(Text);
+            // Создание объекта регулярного выражения
+            // на основе текста
+            //Regex regText = Text.Length == 0 ? null : new Regex(Text, RegexOptions.IgnoreCase);
+            try
+            {
+                // Вызываем функцию поиска
+                ulong Count = FindFilesByDate(start, end, di, regMask);
+                Console.WriteLine("Всего обработано файлов: {0}.", Count);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        // Функция поиска
+        static ulong FindFilesByDate(DateTime start, DateTime end, DirectoryInfo di, Regex regMask)
+        {
+            // Поток для чтения из файла
+            //StreamReader sr = null;
+            // Список найденных совпадений
+            //MatchCollection mc = null;
+
+            // Количество обработанных файлов
+            ulong CountOfMatchFiles = 0;
+
+            FileInfo[] fi = null;
+            try
+            {
+                // Получаем список файлов
+                fi = di.GetFiles();
+            }
+            catch
+            {
+                return CountOfMatchFiles;
+            }
+
+            // Перебираем список файлов
+            foreach (FileInfo f in fi)
+            {
+                // Если файл соответствует маске
+                if (regMask.IsMatch(f.Name) && f.LastWriteTime >= start &&
+                        f.LastWriteTime <= end)
+                {
+                    // Увеличиваем счетчик
+                    ++CountOfMatchFiles;
+                    Console.WriteLine("File " + f.Name);
+                    Console.WriteLine("Файл: {0}, Дата последней модификации: {1}", f.Name, f.LastWriteTime);
+
+                    //if (regText != null)
+                    //{
+                    //    try
+                    //    {
+                    //        // Открываем файл
+                    //        sr = new StreamReader(di.FullName + @"\" + f.Name,
+                    //            Encoding.Default);
+                    //        // Считываем целиком
+                    //        string Content = sr.ReadToEnd();
+                    //        // Закрываем файл
+                    //        sr.Close();
+                    //        // Ищем заданный текст
+                    //        mc = regText.Matches(Content);
+                    //        // Перебираем список вхождений
+                    //        foreach (Match m in mc)
+                    //        {
+                    //            Console.WriteLine("Текст найден в позиции {0}.", m.Index);
+                    //        }
+                    //    }
+                    //    catch (Exception ex)
+                    //    {
+                    //        Console.WriteLine(ex.Message);
+                    //    }
+                    //}
+                }
+            }
+
+            // Получаем список подкаталогов
+            DirectoryInfo[] diSub = di.GetDirectories();
+            // Для каждого из них вызываем (рекурсивно)
+            // эту же функцию поиска
+            foreach (DirectoryInfo diSubDir in diSub)
+                CountOfMatchFiles += FindFilesByDate(start,end, diSubDir, regMask);
+
+            // Возврат количества обработанных файлов
+            return CountOfMatchFiles;
+        }
+    }
+}
